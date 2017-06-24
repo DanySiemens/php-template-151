8880b388105b29da0adb8532c206c641edff32d0
DanySiemens <dany.berufzug@gmail.com>
2017-06-24 22:59:29 +0200

    Update IndexController.php

--- website/src/Controller/IndexController.php
+++ website/src/Controller/IndexController.php
@@ -1,27 +1,15 @@
 <?php
 
 namespace dany\Controller;
-
 use dany\SimpleTemplateEngine;
 use dany\Service\Homepage\HomepageService;
 use dany\Service\Security\CSRFProtectionService;
-
 class IndexController 
 {
-  /**
-   * @var ihrname\SimpleTemplateEngine Template engines to render output
-   */
   private $template;
-  
   private $homepageService;
-  
   private $pdo;
-  
   private $csrfService;
-  
-  /**
-   * @param ihrname\SimpleTemplateEngine
-   */
   public function __construct(SimpleTemplateEngine $template, HomepageService $homepageService, \PDO $pdo, CSRFProtectionService $csrfProtectionService)
   {
      $this->template = $template;
@@ -29,29 +17,21 @@ class IndexController
      $this->pdo = $pdo;
      $this->csrfService = $csrfProtectionService;
   }
-  
   public function homepage() 
   {
   	$postsdb = $this->homepageService->getAllPost();
   	$posts = array();
-  	$counter = 0;
-  	$likesNumber = 0;
-  	$dislikes = 0;
   	if($postsdb != NULL)
   	{
-	  	foreach ($postsdb as $post)
-	  	{
-	  		$likesNumber = 0;
-	  		$dislikes = 0;
-	  		$temp['id'] = $post['id'];
-	  		$temp['user_id'] = $post['user_id'];
-	  		$temp['title'] = $post['title'];
-	  		$temp['content'] = $post['content'];
-	  		$posts[$counter] = $temp;
-	  		$counter++;
-	  	}
+	foreach ($postsdb as $post)
+	{
+	  $temp['id'] = $post['id'];
+	  $temp['user_id'] = $post['user_id'];
+	  $temp['title'] = $post['title'];
+	  $temp['content'] = $post['content'];
+	  }
   	}
-    echo $this->template->render("home.html.php", array('posts' => $posts, 'csrf' => $this->csrfService->getHtmlCode("csrfIndex")));  
+    echo $this->template->render("home.html.php"array('posts'=>$posts,'csrf'=> $this->csrfService->getHtmlCode("csrfIndex")));  
   }
   
   
@@ -65,38 +45,38 @@ class IndexController
   
   public function addPost(array $data)
   {
-  	if(array_key_exists("csrf", $data))
-  	{
-  		if(!$this->csrfService->validateToken("csrfNewPost", $data["csrf"]))
-  		{
-  			$this->showNewPost();
-  			return;
-  		}
-  	}
-  	else
+  if(array_key_exists("csrf", $data))
+  {
+  	if(!$this->csrfService->validateToken("csrfNewPost", $data["csrf"]))
   	{
   		$this->showNewPost();
   		return;
   	}
-  	$this->homepageService->addPost($_SESSION['user_id'],$data["title"], $data["content"]);
-  	header("Location: /");
+  }
+  else
+  {
+  	$this->showNewPost();
+  	return;
+  }
+  $this->homepageService->addPost($_SESSION['user_id'],$data["title"], $data["content"]);
+  header("Location: /");
   }
   
   public function deletePost(array $data)
   {
-  	if(array_key_exists("csrf", $data))
-  	{
-  		if(!$this->csrfService->validateToken("csrfIndex", $data["csrf"]))
-  		{
-  			$this->homepage();
-  			return;
-  		}
-  	}
-  	else
+  if(array_key_exists("csrf", $data))
+  {
+  	if(!$this->csrfService->validateToken("csrfIndex", $data["csrf"]))
   	{
   		$this->homepage();
   		return;
   	}
-  	$this->homepageService->deletePost($data["deletePost"]);
+  }
+  else
+  {
+  	$this->homepage();
+  	return;
+  }
+  $this->homepageService->deletePost($data["deletePost"]);
   }
 }
