7240283a77afab2f3365596ea1891a3970a97dc1
DanySiemens <dany.berufzug@gmail.com>
2017-06-24 22:45:03 +0200

    Update register.html.php

--- website/templates/register.html.php
+++ website/templates/register.html.php
@@ -1,22 +1,17 @@
-<!Doctype>
+<!DOCTYPE html>
 <html>
 <head>
 	<link rel="stylesheet" href="/Design/design.css">
-	<title>Register</title>
+	<title>Registrierung</title>
 </head>
 <body>
 	<h1>Register</h1>
 	<form method="POST">
-	<?php echo $csrf?>
-		<label>
-			Email:
-			<input type="email" name="email" value="<?= (isset($email)) ? htmlspecialchars($email) : "" ?>" />
-		</label>
-		<label>
-			Passwort:
-			<input type="password" name="password">
-		</label>
-		<input type="submit" name="register" value="register">
+	<h2>MailAdresse:</h2>
+	<input type="email" name="email" value="<?= (isset($email)) ? htmlspecialchars($email) : "" ?>" />
+	<h2>Passwort:</h2>
+	<input type="password" name="password">
+	<input type="submit" name="register" value="register">
 	</form>
 	<a href="login">login</a>
 	<a href="https://localhost/">Home</a>
