e4ed3eef45de507ec438f6f46ed13927f8bef4a2
DanySiemens <dany.berufzug@gmail.com>
2017-06-24 22:39:08 +0200

    Update home.html.php

--- website/templates/home.html.php
+++ website/templates/home.html.php
@@ -8,18 +8,20 @@
 <body>
 	<h1>Danys Blog</h1>
 	<?php
-			if(!isset($_SESSION['email'])){
-				echo "<form id='login' method='post'><input type='hidden' name='login' id='login'></input>
-					   	<Button type='submit'>Login</Button>
-				   	  </form>";
-				echo "<form id='register' method='post'><input type='hidden' name='register' id='register'></input>
-					<Button type='submit'>Register</Button>
-      			  </form>";
-			}
-			else {
-			 ?><a href="#">Post Erstellen</a>
-			 <a href="#">Ausloggen</a><?php
-			}
+		if(!isset($_SESSION['email'])){
+		echo "<form id='login' method='post'><input type='hidden' name='login' id='login'></input>
+		<Button type='submit'>Login</Button>
+		</form>";
+		echo "<form id='register' method='post'><input type='hidden' name='register' id='register'></input>
+		<Button type='submit'>Register</Button>
+      		</form>";
+		}
+		else {
+		?><a href="#">Post Erstellen</a>
+		<a href="#">Ausloggen</a><?php
+		}
+		?>
+		<?php
 	?>
 </body>
 </html>
