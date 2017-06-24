e2dcef11976ad4685f8dcf0c5c81f2339908c6c9
DanySiemens <dany.berufzug@gmail.com>
2017-06-24 22:42:09 +0200

    Update login.html.php

--- website/templates/login.html.php
+++ website/templates/login.html.php
@@ -1,22 +1,15 @@
-<!Doctype>
+<!DOCTYPE html>
 <html>
 <head>
 	<link rel="stylesheet" href="/Design/design.css">
  	<title>Login</title>
 </head>
 <body>
-	<h1>Login</h1>
+	<h1>Einloggen</h1>
 	<form method="POST">
 	<?php echo $csrf?>
-		<label>
-			Email:
-			<input type="email" name="email" value="<?= (isset($email)) ? htmlspecialchars($email) : "" ?>" />
-		</label>
-		<label>
-			Passwort:
-			<input type="password" name="password">
-		</label>
-
+		<h2>Email</h2><input type="email" name="email" value="<?= (isset($email)) ? htmlspecialchars($email):""?>"/>
+		<h2>Passwort:</h2><input type="password" name="password">
 		<input type="submit" name="login" value="Login">
 	</form>
 	<a href="register">Registrieren</a>
